796ff8fc13ef8b59e746a055e0c825981c1fedba
Shishir Pandey <shishirpandey087@gmail.com>
2026-07-22 11:52:06 +0545

    Remove Wes Bos inspiration credit from Uses page

--- app/uses/uses-title.tsx
+++ app/uses/uses-title.tsx
@@ -4,17 +4,6 @@ export default function UsesTitle() {
 			<h1 className='text-2xl md:text-4xl font-bold'>
 				Here are the tools I use to build things
 			</h1>
-			<span className='text-gray-700  dark:text-gray-300'>
-				Inspired by{' '}
-				<a
-					href='https://wesbos.com/uses'
-					className='underline-magical'
-					target='_blank'
-					rel='noreferrer'
-				>
-					Wes bos
-				</a>
-			</span>
 		</div>
 	);
 }
